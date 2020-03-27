ee908aa85c42a3f28ac01d076d51928b4d9a6f5c
AlexVulpe99 <44170240+AlexVulpe99@users.noreply.github.com>
2020-03-27 13:26:28 +0200

    update + small changes

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, request,render_template_string, Response
+import time
+app = Flask(__name__)
+
+
+@app.route('/api/question', methods = ['POST'])
+def question():
+    if request.method == 'POST':
+        search_text = request.form['key']
+        if len(search_text) != 0:
+            #fragment = 'asd'
+            return Response("{'success':'true', 'fragment':" + str(fragment) + "}", status=200, mimetype='application/json')
+        else:
+            return Response("{'success':'false', 'message': 'Bad request! Please check the request body'}", status=400, mimetype='application/json')
+
+
+@app.route('/api/question', methods = ['GET'])
+def success():
+    return Response("{'success' : 'true', 'message' : 'Please use the POST method to send the question!!'}", status=200, mimetype='application/json' )
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
