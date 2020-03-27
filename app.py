158b5ee64cee0d0022938f66ce623e240ad8b8ee
AlexVulpe99 <44170240+AlexVulpe99@users.noreply.github.com>
2020-03-27 13:27:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def question():
     if request.method == 'POST':
         search_text = request.form['key']
         if len(search_text) != 0:
-            #fragment = 'asd'
+            fragment = 'asd'
             return Response("{'success':'true', 'fragment':" + str(fragment) + "}", status=200, mimetype='application/json')
         else:
             return Response("{'success':'false', 'message': 'Bad request! Please check the request body'}", status=400, mimetype='application/json')
